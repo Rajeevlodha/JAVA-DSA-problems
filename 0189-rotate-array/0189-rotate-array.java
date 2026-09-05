15ba2db5617354434b59a862e87ccbf6de794520
RAJEEV LODHA <lrajeevlodha@gmail.com>
2026-09-05 19:46:12 +0530

    Time: 5 ms (17.26%), Space: 268.8 MB (10.94%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void rotate(int[] arr, int d) {
+        int n = arr.length;
+        d = d % n;
+        Swap(arr, 0, n - d - 1);
+        Swap(arr, n - d, n - 1);
+        Swap(arr, 0, n - 1);
+    }
+        public static void Swap(int arr[], int st, int end) {
+        while (st < end) {
+            int tmp = 0;
+            tmp = arr[st];
+            arr[st] = arr[end];
+            arr[end] = tmp;
+            st++;
+            end--;
+        }
+    }
+}
