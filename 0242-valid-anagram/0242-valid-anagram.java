1d82d38b3bca9fbb2cf4bd309299b27ca6558496
RAJEEV LODHA <lrajeevlodha@gmail.com>
2026-09-21 14:08:32 +0530

    Time: 4 ms (83.76%), Space: 46.3 MB (45.98%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s1, String s2) {
+        char arr1[] = s1.toCharArray();
+        char arr2[] = s2.toCharArray();
+
+          if(s1.length()!=s2.length()) {
+              return false;
+          }
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+        for (int i = 0; i < s2.length(); i++) {
+            if (arr1[i]!= arr2[i]) {
+                return false;
+                            }
+        }
+        return true;
+    }
+}
